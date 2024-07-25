0b0d4591fd207d3a4cca232061406b514e36d75d
Tomasz Kaliński <tomkalon@gmail.com>
2024-07-25 13:04:30 +0200

    OP-373 Added Sylius 1.13 support

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -67,4 +67,7 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
+    Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
 ];
--- tests/Application/config/sylius/1.13/bundles.php
+++ tests/Application/config/sylius/1.13/bundles.php
@@ -10,7 +10,5 @@
 declare(strict_types=1);
 
 return [
-    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
-    SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
-    Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
+    Sylius\Abstraction\StateMachine\SyliusStateMachineAbstractionBundle::class => ['all' => true],
 ];
